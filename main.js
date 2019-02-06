f69d06fcfef4da40358331fe70825fe54c041fca
Håkon Åreskjold <haareskjold@gmail.com>
2019-02-06 20:00:34 +0100

    comments

--- main.js
+++ main.js
@@ -1,34 +1,37 @@
 $(function(){
     var lag1 = 0;
     var lag2 = 0;
+    var isFullscreen = false;
+
 
     $(document).keypress(function(e) {
-        
+        // Increace score team 1
         if(e.which == 119 || e.which == 87) {
           lag1 = lag1 + 1;
         } 
-        
+
+        // Decrease score team 1
         if(e.which == 83 || e.which == 115) {
             lag1 = lag1 - 1;
 
         }
-
+        // Increace score team 1
         if(e.which == 112 || e.which == 80) {
             lag2 = lag2 + 1;
 
           } 
-          
+          // Decrease score team 1
           if(e.which == 108 || e.which == 76) {
               lag2 = lag2 - 1;
 
           }
-          // nullstiller
+          // Reset points
           if(e.which == 110 || e.which == 78) {
             lag1 = 0;
             lag2 = 0;
         }
     
-        // Fullskjerm
+        // Go fullscreen
         if(e.which == 102 || e.which == 70) {
           if(isFullscreen == false) {
             go_full_screen();
@@ -40,7 +43,7 @@ $(function(){
           
         }
 
-          // Oppdaterer score
+          // Update score after key input
           $(".lag1").html(lag1);
           $(".lag2").html(lag2);
 
@@ -57,8 +60,7 @@ $(function(){
         }, 5000);
       });
     
-      var isFullscreen = false;
-      
+      // go fullscreen
       function go_full_screen(){
         var elem = document.documentElement;
         if (elem.requestFullscreen) {
